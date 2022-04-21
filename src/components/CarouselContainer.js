fe85f47b71341595a46b71cf79ea2ad52ceead23
alejober <99362041+alejober@users.noreply.github.com>
2022-04-21 05:31:47 -0300

    update

--- src/components/CarouselContainer.js
+++ src/components/CarouselContainer.js
@@ -8,6 +8,7 @@ import image5 from './../img/banner-nada-if.png';
 import image6 from './../img/banner-nada-bdt.png';
 import imageBDT from './../img/Logo-bdt-01.png';
 import soluciones from './../img/Logo-Soluciones.png';
+import isologo from './../img/isologo.png';
 
 const CarouselContainer = () => {
 return (
@@ -36,35 +37,17 @@ return (
     />
     <Carousel.Caption>
       <div className='espaciado'>
-        <div className='textazo'>
-      <h1><b>CuantIf</b></h1>
-      </div>
+      <img className='isologo' src={isologo} alt='cuantif'/>
         <div className="alineacion">
           <p>Desarrollamos un software de <b className="negritacuantif">algo-trading automatizado</b> simple e intuitivo, libre de la necesidad de saber programar y flexible a cualquier estrategia de inversión.</p>
-          <a href='http://52.23.241.52:27053/' target='_blank'>
-            <button className='button' type="button" class="btn btn-outline-light">Descubri más sobre <b>Quantif</b></button>
+          <a href='http://www.cuantif.com/' target='_blank'>
+            <button className='button' type="button" class="btn btn-outline-light">Descubri más sobre <b>Cuantif</b></button>
           </a>
         </div>
       </div>
     </Carousel.Caption>
   </Carousel.Item > 
 
-  <Carousel.Item class='carouselBloque' interval={5000}>
-    <img
-      className="d-block w-100"
-      src={image6}
-      alt="bdt"
-    />
-    <Carousel.Caption>
-    <div className='espaciado'>
-      <img className='imagenBDT' src={imageBDT} alt='imageBDT'/>
-      <div className="alineacion">
-      <p>Traemos a la región <b>las estrategias de trading y cobertura más avanzadas</b>, dinámicas y sofisticadas sobre el instrumento financiero mas seguro del mundo, benchmark de la economía mundial.</p>
-      <a href='https://bonosdeltesoroestadosunidos.com/' target='_blank'><button className='button' type="button" class="btn btn-outline-light">Ir a <b>BdT.US</b></button></a>
-      </div>
-    </div>
-    </Carousel.Caption>
-  </Carousel.Item>
   <Carousel.Item class='carouselBloque' interval={5000}>
     <img
       className="d-block w-100"
@@ -78,6 +61,22 @@ return (
       <p>Como resultado de nuestras investigaciones financieras y desarrollos tecnológicos, estamos <b>constantemente tomando posiciones en los mercados de trading de plataforma</b>.</p>
       {/* <button className='button' type="button" class="btn btn-outline-light">Descubri más sobre <b>Señales</b></button> */}
     </div>
+    </div>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item class='carouselBloque' interval={5000}>
+    <img
+      className="d-block w-100"
+      src={image6}
+      alt="bdt"
+    />
+    <Carousel.Caption>
+    <div className='espaciado'>
+      <img className='imagenBDT' src={imageBDT} alt='imageBDT'/>
+      <div className="alineacion">
+      <p>Traemos a la región <b>las estrategias de trading y cobertura más avanzadas</b>, dinámicas y sofisticadas sobre el instrumento financiero mas seguro del mundo, benchmark de la economía mundial.</p>
+      <a href='https://bonosdeltesoroestadosunidos.com/' target='_blank'><button className='button' type="button" class="btn btn-outline-light">Ir a <b>BdT.US</b></button></a>
+      </div>
     </div>
     </Carousel.Caption>
   </Carousel.Item>
